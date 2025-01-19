ec0cde6883e0068b628b743659925f78b53fe743
René 'Necoro' Neumann <necoro@necoro.eu>
2025-01-19 22:40:24 +0100

    Read JES file

--- main.go
+++ main.go
@@ -1,10 +1,37 @@
 package main
 
 import (
-	"compress/flate"
+	"archive/zip"
+	"encoding/xml"
 	"log"
 	"os"
 )
+
+type Eur struct {
+	XmlName  xml.Name  `xml:"eur"`
+	Name     string    `xml:"general>name"`
+	Address  string    `xml:"general>address"`
+	Company  string    `xml:"general>company"`
+	TaxID    string    `xml:"general>taxid"`
+	Start    Date      `xml:"general>businessyearrange>daterange>start>date"`
+	Receipts []Receipt `xml:"receipts>receipt"`
+}
+
+type Date struct {
+	Year  string `xml:"year,attr"`
+	Month string `xml:"month,attr"`
+	Day   string `xml:"day,attr"`
+}
+
+type Receipt struct {
+	Date     Date   `xml:"date"`
+	Incoming string `xml:"payment>taxaccountincoming"`
+	Outgoing string `xml:"payment>taxaccountoutgoing"`
+	Amount   struct {
+		TaxHandling string `xml:"tax,attr"`
+		Value       string `xml:",chardata"`
+	} `xml:"payment>amount"`
+}
 
 func main() {
 	log.SetFlags(0)
@@ -15,12 +42,24 @@ func main() {
 	jesFile := os.Args[1]
 	//month := os.Args[2]
 
-	f, err := os.Open(jesFile)
+	zipF, err := zip.OpenReader(jesFile)
 	if err != nil {
 		log.Fatalf("Opening file '%s': %v", jesFile, err)
 	}
+	defer zipF.Close()
+
+	f, err := zipF.File[0].Open()
+	if err != nil {
+		log.Fatalf("Decompressing file '%s': %v", jesFile, err)
+	}
 	defer f.Close()
 
-	deflateReader := flate.NewReader(f)
-	defer deflateReader.Close()
+	var eur Eur
+	decoder := xml.NewDecoder(f)
+	err = decoder.Decode(&eur)
+	if err != nil {
+		log.Fatalf("Decoding '%s': %v", jesFile, err)
+	}
+
+	log.Printf("%+v", eur)
 }
